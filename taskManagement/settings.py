104eba83383f486c516a54181a2b28bbc6462fbd
connorshaw <chshaw93@gmail.com>
2023-05-05 10:52:08 -0300

    Added initial django website files and app

--- taskManagement/settings.py
+++ taskManagement/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'taskManagementApp',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'taskManagement.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['taskManagementApp/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
