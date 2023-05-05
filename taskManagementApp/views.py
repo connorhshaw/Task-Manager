c570f5c127398ce0b06f6c53fc8beebc5e856fe6
connorshaw <chshaw93@gmail.com>
2023-05-05 12:38:05 -0300

    Added task list functionality and bootstrap

--- taskManagementApp/views.py
+++ taskManagementApp/views.py
@@ -1,4 +1,28 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from .models import TaskDb
+from .forms import TaskForm
+from django.contrib import messages
 
 def home(request):
-    return render(request, 'index.html')
+    
+    if request.method == 'POST':
+        form = TaskForm(request.POST or None)
+
+        if form.is_valid():
+            form.save()
+            all_items = TaskDb.objects.all()
+            messages.success(request, ("New item added"))
+            return HttpResponseRedirect(reverse('home'))
+        
+    else:
+        all_items = TaskDb.objects.all()
+        return render(request, 'index.html', {'all_items':all_items})
+
+def delete(request, list_id):
+
+    item = TaskDb.objects.get(pk=list_id)
+    item.delete()
+    messages.success(request, ('Item deleted'))
+    return redirect('home')
